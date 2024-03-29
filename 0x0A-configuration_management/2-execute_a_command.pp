aa12533f6e19e4e6f2ed63e609db547b8d258f57
markdrahg <neutral520@gmail.com>
2024-03-29 07:03:59 +0000

    CHANGE 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-lls a process
-exec { 'killmenow':
-  command  => '/usr/bin/pkill killmenow',
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
   provider => 'shell',
-  returns  => [0, 1],
 }
+
