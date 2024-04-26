cbde96fd9f7ae37f564ed75a905375d90edd75ec
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-04-26 22:12:32 +0000

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Installs puppet-lint
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Ensure Werkzeug is installed with a compatible version
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
