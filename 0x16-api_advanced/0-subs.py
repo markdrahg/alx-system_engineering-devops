fd7946aa1ff3e7ab55c54afba6a6fed75db4efb4
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-05-09 18:21:16 -0700

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"subscribers count"
+"""
+ Queries the Reddit API and returns the number of subscribers.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """Returns the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    if response.status_code != 200:
-        return 0
-    else:
+
+    if response.status_code == 200:
+        data = response.json()
         return data['data']['subscribers']
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    subreddit = sys.argv[1] if len(sys.argv) > 1 else None
+    if subreddit:
+        print(number_of_subscribers(subreddit))
+    else:
+        print("Please pass an argument for the subreddit to search.")
